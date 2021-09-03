a0787963678e35d0a30d9164d840741f47d7a6f7
Jack Linhart <iatenine@gmail.com>
2021-09-03 14:28:11 -0600

    Boilerplate dir structure

---
+++ server.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+// const SequelizeStore = require("connect-session-sequelize")(session.Store);
+// const sequelize = require("./config/connection");
+// const helpers = require("./utils/helpers");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => console.log("Now listening"));
