5a827c209d6f8f7a5b681f74b28dd3ef66840b90
Gregory Friel <87154134+Friel2021@users.noreply.github.com>
2021-09-09 15:16:24 -0600

    leave the s'

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use(controllers);sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+// app.use(controllers);
 app.use(controllers);
 
 sequelize.sync({ force: false }).then(() => {
