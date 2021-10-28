98ac9198e4990f522bb970ff149c639dee0933e7
Jack Linhart <iatenine@gmail.com>
2021-10-27 22:46:49 -0600

    add route and button listener

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -86,6 +86,24 @@ router.put("/:id", async (req, res) => {
   }
 });
 
+// Protected put route to update user profile
+router.put("/updateProfile", async (req, res) => {
+  if (req?.session?.loggedIn) res.status(403).end();
+  try {
+    const user = await User.findByPk(req.session.userId);
+    // strip all items except steamid and zipcode from req.body
+    const sanitizedBody = {
+      steamid: req.body?.steamid ? req.body.steamid : user.steamid,
+      zipcode: req.body?.zipcode ? req.body.zipcode : user.zipcode,
+    };
+    const result = await user.update(sanitizedBody);
+    res.status(201).json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
+
 router.get("/library/:id", async (req, res) => {
   try {
     const library = await User.findOne({
--- public/js/updateProfile.js
+++ public/js/updateProfile.js
@@ -48,6 +48,32 @@ const updateButton = () => {
     updateProfileButton.removeClass("disabled");
 };
 
+updateProfileButton.click(updateProfile);
+
+const updateProfile = () => {
+    const newZip = zipcodeInput.val();
+    const newUID = uidInput.val();
+
+    // Only enable update button if all inputs are valid
+    if(isZipCodeValid(newZip) && !isNaN(newUID) && newUID.length === 17) {
+        $.ajax({
+            url: "/updateProfile",
+            type: "PUT",
+            data: {
+                zipcode: newZip,
+                steamId: newUID
+            },
+        }).done(function(data) {
+            if(data.success) {
+                window.location.href = "/profile";
+            }
+        }).fail(
+            function(data) {
+                console.error(data);
+            }
+        )
+}
+
 // Just some inline test-driven development. Nothing to see here.
 console.assert(isZipCodeValid("H0H 0H0") === true);
 console.assert(isZipCodeValid("40H 0H0") === false);
